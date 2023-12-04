925f29f4d6a06cd750767bfb5b552202b57babbb
Abhinav Koyyalamudi <90361279+aKoyyla@users.noreply.github.com>
2023-12-04 16:46:33 -0700

    Fixed issues with clients,params

--- Backend/API/app.js
+++ Backend/API/app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getMarketHistory } = require('../DBQueries/StockMarketQueries');
+const { getMarketHistory, getStock, getUserHistory, InsertUserHistory, DelUserHistory } = require('../DBQueries/StockMarketQueries');
 console.log( require('../DBQueries/StockMarketQueries'))
 
 
@@ -7,13 +7,13 @@ console.log( require('../DBQueries/StockMarketQueries'))
 //Just a basic api endpoint I made
 const app1 = express();
 
-/*
-app.get('/', (req, res) => {
+
+app1.get('/', (req, res) => {
   res.send('Api is working');
 });
 
-*/
-app1.get('/', async (req, res) => {
+
+app1.get('/marketH', async (req, res) => {
   //res.send('Api is working2');
   try {
     const limit = 20; //default limit
@@ -26,6 +26,85 @@ app1.get('/', async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 });
+
+app1.get('/getStock/:param1', async (req, res) => {
+  //res.send('Api is working2');
+  try {
+    const abbrv1 = req.params.param1;
+    //const date1 = req.params.param2;
+    const abbrv = 'AAL'; //default
+    const date = '2/7/18';
+    
+    const stock = await getStock(abbrv1, date);
+
+    const dataArray = stock.map(record => Object.values(record));
+    res.send(dataArray);
+   // res.send('Api is working2');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app1.get('/getUH/', async (req, res) => {
+  //res.send('Api is working2');
+  try {
+    //const abbrv = 'AAL'; //default
+    //const date = '2/7/18';
+
+ 
+    const stock = await getUserHistory() ;
+
+    const dataArray = stock.map(record => Object.values(record));
+    res.send(dataArray);
+   // res.send('Api is working2');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app1.get('/InsertUH/:p1/:p2/:p3/:p4', async (req, res) => {
+  //res.send('Api is working2');
+  try {
+    const abbv = 'AAL'; //default
+    const date = '2/7/18';
+    const price = '13.565';
+    const volume = '64546';
+   
+    const abbv1 = req.params.p1;
+    const date1 =  req.params.p2;
+    const price1 = req.params.p3;
+    const volume1 =  req.params.p4;
+
+    const stock = await InsertUserHistory(abbv1, date1, price1, volume1) ;
+
+    const dataArray = stock.map(record => Object.values(record));
+    res.send(dataArray);
+   // res.send('Api is working2');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app1.get('/DelUH/:p1/:p2', async (req, res) => {
+  //res.send('Api is working2');
+  try {
+    const abbv = 'AAL'; //default
+    const date = '2/7/18';
+
+    const abbv1 = req.params.p1;
+    const date1 =  req.params.p2;
+    
+    const ret = await DelUserHistory(abbv1, date1) ;
+
+    const dataArray = ret.map(record => Object.values(record));
+    res.send(dataArray);
+   // res.send('Api is working2');
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
 
 // Start the server
 const port = process.env.PORT || 8763; // Use the port specified in environment variables or default to 3000
--- Backend/DBQueries/StockMarketQueries.js
+++ Backend/DBQueries/StockMarketQueries.js
@@ -3,13 +3,7 @@ const {Client } = require('pg');
 
 //Configuration to connect with groupDB
 
-const dbClient  = new Client ({
-    user: 'abhinavkoyyalamudi',
-    host: 'localhost',
-    database: 'abhinavkoyyalamudi',
-    port: 8889,
-    // Might have to add a password, dk if we have 1 set
-  }); 
+
 
 
   //dbClient.connect().then(() => console.log('Connected to GroupDatabase'))
@@ -19,11 +13,22 @@ const dbClient  = new Client ({
 
 // We can write sample query functions like this
 async function getMarketHistory(limit = 20) {
+  const dbClient  = new Client ({
+    user: 'abhinavkoyyalamudi',
+    host: 'localhost',
+    database: 'abhinavkoyyalamudi',
+    port: 8889,
+    // Might have to add a password, dk if we have 1 set
+  }); 
+
+  
+
     try {
       console.log('db client is running');
       await dbClient.connect();
       const result = await dbClient.query('SELECT * FROM marketHistory limit ' + limit);
       console.log(result.rows);
+      await dbClient.end();
       return result.rows;
     } catch (error) {
       console.log(error.message);
@@ -35,12 +40,125 @@ async function getMarketHistory(limit = 20) {
   }
 
   //Insert Into user history
+  async function InsertUserHistory(abbv, date, price, volume) {
+    const dbClient  = new Client ({
+      user: 'abhinavkoyyalamudi',
+      host: 'localhost',
+      database: 'abhinavkoyyalamudi',
+      port: 8889,
+      // Might have to add a password, dk if we have 1 set
+    }); 
+
+
+    try {
+      console.log('db client is running');
+      await dbClient.connect();
+      const result = await dbClient.query('insert into userHistory (u_abbv, u_date, u_price, u_volume) VALUES (\'' + abbv + '\',\''+ date + '\',\'' +  price + '\',\'' + volume + '\')');
+      console.log(result.rows);
+      await dbClient.end();
+      return result.rows;
+    } catch (error) {
+      console.log(error.message);
+      console.log('db client is not running');
+      throw new Error('Error fetching users: ' + error.message);
+    } finally {
+      await dbClient.end();
+    }
+  }
+
+
+   //Delete fro, user history
+   async function DelUserHistory(abbv, date) {
+    const dbClient  = new Client ({
+      user: 'abhinavkoyyalamudi',
+      host: 'localhost',
+      database: 'abhinavkoyyalamudi',
+      port: 8889,
+      // Might have to add a password, dk if we have 1 set
+    }); 
+
+
+    try {
+      console.log('db client is running');
+      await dbClient.connect();
+      const result = await dbClient.query('DELETE FROM userHistory where u_abbv = \'' + abbv + '\' AND u_date = \'' + date + '\'');
+      console.log(result.rows);
+
+      await dbClient.end();
+      return result.rows;
+    } catch (error) {
+      console.log(error.message);
+      console.log('db client is not running');
+      await dbClient.end();
+      throw new Error('Error fetching users: ' + error.message);
+    } finally {
+      await dbClient.end();
+    }
+  }
+  
+  
 
   //Get all of user history table
+  async function getUserHistory() {
+    const dbClient  = new Client ({
+      user: 'abhinavkoyyalamudi',
+      host: 'localhost',
+      database: 'abhinavkoyyalamudi',
+      port: 8889,
+      // Might have to add a password, dk if we have 1 set
+    }); 
+
+
+    try {
+      console.log('db client is running');
+      await dbClient.connect();
+      const result = await dbClient.query('SELECT * FROM userHistory');
+      console.log(result.rows);
+      await dbClient.end();
+      return result.rows;
+    } catch (error) {
+      console.log(error.message);
+      console.log('db client is not running');
+      await dbClient.end();
+      throw new Error('Error fetching users: ' + error.message);
+    } finally {
+      await dbClient.end();
+    }
+  }
 
   //Given abbv, return the price of the stock on date '2/7/2018'
+  async function getStock(abrev, date1) {
+    const dbClient  = new Client ({
+      user: 'abhinavkoyyalamudi',
+      host: 'localhost',
+      database: 'abhinavkoyyalamudi',
+      port: 8889,
+      // Might have to add a password, dk if we have 1 set
+    }); 
+
+
+    try {
+      console.log('db client is running');
+      await dbClient.connect();
+      const result = await dbClient.query('SELECT m_price, m_volume FROM marketHistory where m_abbv = \'' + abrev + '\' AND m_date = \'' + date1 + '\'');
+      console.log(result.rows);
+      await dbClient.end();
+      return result.rows;
+    } catch (error) {
+      console.log(error.message);
+      console.log('db client is not running');
+      await dbClient.end();
+      throw new Error('Error fetching users: ' + error.message);
+    } finally {
+      await dbClient.end();
+    }
+  }
   
   // Export the functions
   module.exports = {
-    getMarketHistory
+    getMarketHistory,
+    getUserHistory,
+    getStock,
+    InsertUserHistory,
+    DelUserHistory
   };
